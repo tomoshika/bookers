934e855be7aa7b0e80af03297e1e6edecb38ea2d
tomoshika <tmm.orange.skm@gmail.com>
2021-08-07 07:29:48 +0000

    [Update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book)
+    redirect_to book_path(book), notice: "Book was successfully created."
   end
 
   def show
@@ -16,19 +16,19 @@ class BooksController < ApplicationController
   end
 
   def edit
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
   
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: "Book was successfully destroyed."
   end
   
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to show_book_path(book.id)
+    redirect_to book_path(book.id), notice: "Book was successfully updated."
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'book#update', as: 'update_book'
-  put 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy', as: 'destroy'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
