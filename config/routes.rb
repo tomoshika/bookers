d2aee1b58843da20a409eb395c109203cef770ca
tomoshika <tmm.orange.skm@gmail.com>
2021-08-07 03:52:04 +0000

    [Update]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'book#update', as: 'update_book'
+  put 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy', as: 'destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
