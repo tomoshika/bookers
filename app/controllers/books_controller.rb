620c35d7761a55be43661d351dda4e6c97902c50
tomoshika <tmm.orange.skm@gmail.com>
2021-08-07 15:03:34 +0000

    [Update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,12 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book), notice: "Book was successfully created."
+    @book = Book.new(book_params)
+   if @book.save
+    redirect_to book_path(@book), notice: "Book was successfully created."
+   else
+    render :index
+   end
   end
 
   def show
